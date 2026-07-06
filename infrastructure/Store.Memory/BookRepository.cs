d2671f631fa5f0f34dafd73254366608a9f9fbb1
DVOsh <oshur96@gmail.com>
2026-07-06 05:31:02 +0300

    Реализоваать поиск по ISBN  и автору

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -4,14 +4,22 @@
     {
         private readonly Book[] books =
         [
-            new Book(1, "Art Of Programming"),
-            new Book(2, "Refactoring"),
-            new Book(3, "C Programming Language"),
+            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art Of Programming"),
+            new Book(2, "ISBN 13212-32121", "M. Fowler", "Refactoring"),
+            new Book(3, "ISBN 32131-13212", "B. Kernighan, D. Ritchie", "C Programming Language"),
         ];
 
-        public Book[] GetAllByTitle(string titlePart)
+        public Book[] GetAllByIsbn(string isbn)
         {
-            return [.. books.Where(book => book.Title.Contains(titlePart))];
+            return books.Where(book => book.Isbn == isbn)
+                .ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string titlePart)
+        {
+            return books.Where(book => book.Author.Contains(titlePart) 
+                                    || book.Title.Contains(titlePart))
+                        .ToArray();
         }
     }
 }
