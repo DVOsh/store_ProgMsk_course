2e5a4f63927526427be43a81ccb7b69994e3c3c8
DVOsh <oshur96@gmail.com>
2026-07-07 19:15:53 +0300

    Добавить кнопки удаления товара из корзины

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -53,18 +53,18 @@ namespace Store.Web.Controllers
 
             SaveOrderAndCart(order, cart);
 
-            return RedirectToAction("Index", "Book", new { bookId });
+            return RedirectToAction("Index", "Order");
         }
 
-        public IActionResult RemoveItem(int id)
+        public IActionResult RemoveItem(int bookId)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
 
-            order.RemoveItem(id);
+            order.RemoveItem(bookId);
 
             SaveOrderAndCart(order, cart);
 
-            return RedirectToAction("Index", "Book", new { id });
+            return RedirectToAction("Index", "Order");
         }
 
         private void SaveOrderAndCart(Order order, Cart cart)
