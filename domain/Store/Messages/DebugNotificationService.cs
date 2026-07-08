4d1842c2789016c612b4876872b286eead4ea0a2
DVOsh <oshur96@gmail.com>
2026-07-08 15:06:04 +0300

    Выбирать способ доставки и вводить параметры доставки. INotificationService и DevugNotificationService восстановлены!

---
+++ domain/Store/Messages/DebugNotificationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Store.Messages
+{
+    public class DebugNotificationService : INotificationService
+    {
+        public void SendConfirmationCode(string cellPhone, int code)
+        {
+            Debug.WriteLine("Cell phone: {0}, code: {1:0000}.", cellPhone, code);
+        }
+
+        public void StartProcess(Order order)
+        {
+            Debug.WriteLine("Order ID {0}", order.Id);
+        }
+    }
+}
--- presentation/Store.Web/Models/ConfirmationModel.cs
+++ presentation/Store.Web/Models/ConfirmationModel.cs
@@ -5,5 +5,7 @@
         public int OrderId { get; set; }
         
         public string CellPhone { get; set; }
+
+        public Dictionary<string, string> Errors { get; set; } = new Dictionary<string, string>();
     }
 }
