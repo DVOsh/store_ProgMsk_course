b3536eb7209feb9184541356a808a72442729302
DVOsh <oshur96@gmail.com>
2026-07-12 19:30:31 +0300

    Включить миграции

---
+++ infrastructure/Store.Data.EF/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Store.Data.EF
+{
+    public class BookRepository : IBookRepository
+    {
+        public Book[] GetAllByIds(IEnumerable<int> bookIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string titleOrAuthor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ infrastructure/Store.Data.EF/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Data.EF
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Order Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
