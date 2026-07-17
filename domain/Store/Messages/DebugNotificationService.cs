2a144ffd9ae04e1f2d15b313c5e962fe3f0df046
DVOsh <oshur96@gmail.com>
2026-07-17 23:21:35 +0300

    Закомментировал отправку сообщения о новом зааказе на email

--- domain/Store/Messages/DebugNotificationService.cs
+++ domain/Store/Messages/DebugNotificationService.cs
@@ -17,19 +17,19 @@ namespace Store.Messages
         {
             Debug.WriteLine("Order ID {0}", order.Id);
 
-            using var client = new SmtpClient();
-            var message = new MailMessage("from@at.my.domain", "to@at.my.domain");
-            message.Subject = "Заказ #" + order.Id;
+            //using var client = new SmtpClient();
+            //var message = new MailMessage("from@at.my.domain", "to@at.my.domain");
+            //message.Subject = "Заказ #" + order.Id;
 
-            var builder = new StringBuilder();
-            foreach(var item in order.Items)
-            {
-                builder.Append("{0}, {1}", item.BookId, item.Count);
-                builder.AppendLine();
-            }
+            //var builder = new StringBuilder();
+            //foreach(var item in order.Items)
+            //{
+            //    builder.Append("{0}, {1}", item.BookId, item.Count);
+            //    builder.AppendLine();
+            //}
 
-            message.Body = builder.ToString();
-            client.Send(message);
+            //message.Body = builder.ToString();
+            //client.Send(message);
         }
     }
 }
